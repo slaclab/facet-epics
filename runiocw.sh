55065d0610d140ebe6dee18ca5f5ccec950526a3
SLAC fphysics <whitegr@slac.stanford.edu>
2021-03-20 10:35:12 -0700

    restarts already running app

--- runiocw.sh
+++ runiocw.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-ssh facet-srv02 "cd ~/whitegr/facet-sw/epics/ioc/iocBoot/ioccathodeSupportSim; screen -L -dmS iocCathodeSupport ../../bin/linux-x86_64/cathodeSupportSim st.cmd"
+if [ "`ssh facet-srv02 screen -list | grep -w iocCathodeSupport | wc -l`" != "0" ]
+  then
+    echo "CathodeSupport IOC already running, restarting..."
+    `/usr/local/facet/tools/epics/killiocw.sh` 
+fi
+ssh facet-srv02 "cd /usr/local/facet/tools/epics/ioc/iocBoot/ioccathodeSupportSim; screen -L -dmS iocCathodeSupport ../../bin/linux-x86_64/cathodeSupportSim st.cmd"
 
